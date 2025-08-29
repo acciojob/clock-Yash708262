c3da1423f62c2cb203622955789b361f867214dd
Yash708262 <yash708262@gmail.com>
2025-08-29 11:13:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+  const timer = document.getElementById("timer");
+  const now = new Date(); // abhi ka date-time lo
+
+  // Date aur time ko readable format me convert karo
+  const dateStr = now.toLocaleDateString(); 
+  const timeStr = now.toLocaleTimeString();
+
+  timer.textContent = dateStr + " " + timeStr;
