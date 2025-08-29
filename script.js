74cad7783d7e2d48dde36bf5dbe87cac8a6143d5
Yash708262 <yash708262@gmail.com>
2025-08-29 11:20:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-function updateTimer() {
-  const timer = document.getElementById("timer");
-  const now = new Date(); // abhi ka date-time lo
+let timerDisplay = document.getElementById('timer');
 
-  // Date aur time ko readable format me convert karo
-  const dateStr = now.toLocaleDateString(); 
-  const timeStr = now.toLocaleTimeString();
-
-  timer.textContent = dateStr + " " + timeStr;
-}
-
-// har 1 sec me updateTimer function call hoga
-setInterval(updateTimer, 1000);
-
-// page load hote hi turant show ho jaye
-updateTimer();
+setInterval(function() {
+    let currentDate = new Date();
+    timerDisplay.innerHTML = currentDate.toLocaleDateString() + " " + currentDate.toLocaleTimeString();
+}, 1000);
